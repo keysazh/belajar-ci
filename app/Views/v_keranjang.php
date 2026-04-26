96278faa9237fc5c9e4e832f66507dbcb4493028
keysazh <keisazahira106@gmail.com>
2026-04-26 09:20:12 +0700

    Menambahkan fitur filter Auth pada halaman keranjang dan produk

--- app/Views/v_keranjang.php
+++ app/Views/v_keranjang.php
@@ -1,1 +1,6 @@
-Ini halaman keranjang
+<?= $this->extend('layout') ?>
+
+<?= $this->section('content') ?>
+    <h5 class="card-title">Keranjang</h5>
+    <p>Ini halaman keranjang</p>
+<?= $this->endSection() ?>
