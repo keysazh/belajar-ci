6b6c1e4acc27ded3cc6f1b3625888522e7fa9a50
keysazh <keisazahira106@gmail.com>
2026-05-27 05:55:17 +0700

    Menyelesaikan tugas Capstone7: Read Data dan Validasi Produk

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -5,36 +5,37 @@ namespace App\Controllers;
 use App\Controllers\BaseController;
 use CodeIgniter\HTTP\ResponseInterface;
 
+use App\Models\UserModel; 
+
 class AuthController extends BaseController
 {
+    protected $userModel;
+
     function __construct()
 {
     helper('form');
+    $this->userModel = new UserModel();
 }
 
 public function login()
 {
     if ($this->request->getPost()) {
+     $rules = [
+     'username' => 'required|min_length[6]',
+     'password' => 'required|min_length[7]|numeric',
+     ];
+
+    if ($this->validate($rules)) {
         $username = $this->request->getVar('username');
         $password = $this->request->getVar('password');
 
-        // Data dummy untuk login
-        $dataUser = [
-            'username' => 'april', 
-            'password' => '202cb962ac59075b964b07152d234b70', 
-            'role'     => 'admin',
-            'email'    => 'april@example.com'
-        ];
+        $dataUser = $this->userModel ->where(['username' => $username])->first();
 
-        if ($username == $dataUser['username']) {
-            if (md5($password) == $dataUser['password']) {
-                // SET SESSION LENGKAP DI SINI
+        if ($dataUser) {
+	        if (password_verify($password, $dataUser['password'])) {
                 session()->set([
                     'username'    => $dataUser['username'],
                     'role'        => $dataUser['role'],
-                    'email'       => $dataUser['email'],
-                    'waktu_login' => date('Y-m-d H:i:s'),
-                    'log'         => TRUE,
                     'isLoggedIn'  => TRUE
                 ]);
 
@@ -47,6 +48,10 @@ public function login()
             session()->setFlashdata('failed', 'Username Tidak Ditemukan');
             return redirect()->back();
         }
+        } else {
+    session()->setFlashdata('failed', $this->validator->listErrors());
+    return redirect()->back();
+}
     } else {
         return view('v_login');
     }
